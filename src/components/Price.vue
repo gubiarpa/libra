5ce2851e333e97dfdec702e10b5189e2c7e52db8
Billy Arredondo <billy.arredondo@pucp.pe>
2021-09-25 20:39:38 -0500

    consulta, respuesta sections

--- src/components/Price.vue
+++ src/components/Price.vue
@@ -1,32 +1,54 @@
 <template>
     <div>
-        <!-- Textbox Price -->
         <v-col
+            class="mt-5"
             cols="12"
             sm="6"
             md="3"
         >
+            <h3 class="mb-4">
+                Consulta
+            </h3>
+            <!-- Price -->
             <v-text-field
                 label="Precio"
                 type="tel"
                 outlined
             >
             </v-text-field>
-        </v-col>
-        
-        <!-- Unit -->
-        <v-col
-            cols="12"
-            sm="6"
-            md="3"
-        >
+            <!-- Unit -->
             <v-select
                 label="Unidad"
                 :items="units"
                 v-model="selectedUnit"
                 item-text="name"
                 outlined
-                ></v-select>
+            ></v-select>
+
+            <v-divider
+                class="mb-6"
+            ></v-divider>
+
+            <h3 class="mb-4">
+                Respuesta
+            </h3>
+
+            <!-- Target Unit -->
+            <v-select
+                label="Unidad Objetivo"
+                :items="units"
+                v-model="selectedTargetUnit"
+                item-text="name"
+                outlined
+            ></v-select>
+            <!-- Price -->
+            <v-text-field
+                label="Precio Objetivo"
+                type="tel"
+                outlined
+                readonly
+            >
+            </v-text-field>
         </v-col>
     </div>
 </template>
@@ -37,6 +59,7 @@ export default {
         return {
             price: null,
             selectedUnit: 'Kilogramo (g)',
+            selectedTargetUnit: 'Kilogramo (g)',
             units: [
                 'Kilogramo (Kg)',
                 'Gramo (g)'
