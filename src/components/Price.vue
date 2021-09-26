a76d954ebd43eed1003af2dd38f3a48834835e18
Billy Arredondo <billy.arredondo@pucp.pe>
2021-09-25 21:21:43 -0500

    wrap controls - part 1

--- src/components/Price.vue
+++ src/components/Price.vue
@@ -3,27 +3,28 @@
         <v-col
             class="mt-5"
             cols="12"
-            sm="6"
-            md="3"
         >
             <h3 class="mb-4">
-                Consulta
+                Precio por
             </h3>
-            <!-- Price -->
-            <v-text-field
-                label="Precio"
-                type="tel"
-                outlined
-            >
-            </v-text-field>
-            <!-- Unit -->
-            <v-select
-                label="Unidad"
-                :items="units"
-                v-model="selectedUnit"
-                item-text="name"
-                outlined
-            ></v-select>
+            <v-row>
+                <!-- Price -->
+                <v-text-field
+                    label="Precio por Kg"
+                    type="tel"
+                    outlined
+                    cols="3"
+                >
+                </v-text-field>
+                <!-- Unit -->
+                <v-select
+                    label="Unidad"
+                    :items="units"
+                    v-model="selectedUnit"
+                    item-text="name"
+                    outlined
+                ></v-select>
+            </v-row>
 
             <v-divider
                 class="mb-6"
@@ -32,23 +33,27 @@
             <h3 class="mb-4">
                 Respuesta
             </h3>
+            <v-row>
 
-            <!-- Target Unit -->
-            <v-select
-                label="Unidad Objetivo"
-                :items="units"
-                v-model="selectedTargetUnit"
-                item-text="name"
-                outlined
-            ></v-select>
-            <!-- Price -->
-            <v-text-field
-                label="Precio Objetivo"
-                type="tel"
-                outlined
-                readonly
-            >
-            </v-text-field>
+                
+
+                <!-- Target Unit -->
+                <v-select
+                    label="Unidad Objetivo"
+                    :items="units"
+                    v-model="selectedTargetUnit"
+                    item-text="name"
+                    outlined
+                ></v-select>
+                <!-- Price -->
+                <v-text-field
+                    label="Precio Objetivo"
+                    type="tel"
+                    outlined
+                    readonly
+                >
+                </v-text-field>
+            </v-row>
         </v-col>
     </div>
 </template>
