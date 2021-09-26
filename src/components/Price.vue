a64edcdce42c6377ab2206ecd3db2518fc814c79
Billy Arredondo <billy.arredondo@pucp.pe>
2021-09-25 20:24:48 -0500

    combo unit added

--- src/components/Price.vue
+++ src/components/Price.vue
@@ -1,21 +1,48 @@
 <template>
-  <v-col
-    cols="12"
-    sm="6"
-    md="3"
-  >
-    <v-text-field
-        label="Precio"
-        type="tel"
-        outlined
-    >
-    </v-text-field>
-    </v-col>
+    <div>
+        <!-- Textbox Price -->
+        <v-col
+            cols="12"
+            sm="6"
+            md="3"
+        >
+            <v-text-field
+                label="Precio"
+                type="tel"
+                outlined
+            >
+            </v-text-field>
+        </v-col>
+        
+        <!-- Unit -->
+        <v-col
+            cols="12"
+            sm="6"
+            md="3"
+        >
+            <v-select
+                label="Unidad"
+                :items="units"
+                v-model="selectedUnit"
+                item-text="name"
+                outlined
+                ></v-select>
+        </v-col>
+    </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            price: null,
+            selectedUnit: 'Kilogramo (g)',
+            units: [
+                'Kilogramo (Kg)',
+                'Gramo (g)'
+            ],
+        }
+    },
 }
 </script>
 
